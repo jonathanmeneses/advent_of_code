cc9e890ecdd4e7028569c66a5e71b483fc6c158a
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-12-04 21:52:18 -0600

    solved day 2 challenges

--- day_2/challenge_2.rb
+++ day_2/challenge_2.rb
@@ -5,31 +5,29 @@ input_data = File.read(input_file_path)
 # Parse each line into an array of integers
 lines = input_data.split("\n").map { |line| line.split.map(&:to_i) }
 
-def valid_line?(line, direction)
-  (1...line.length).all? do |i|
-    diff = direction == :increasing ? line[i] - line[i-1] : line[i-1] - line[i]
+def valid_line?(line)
+  check1 = (1...line.length).all? do |i|
+    diff = line[i] - line[i-1]
     diff.between?(1, 3)
   end
+  check2 = (1...line.length).all? do |i|
+    diff = line[i-1] - line[i]
+    diff.between?(1, 3)
+  end
+  check1 || check2
 end
 
-def valid_with_one_removal?(line, direction)
+def valid_with_one_removal?(line)
   line.each_with_index.any? do |_, index|
     new_line = line.dup
     new_line.delete_at(index)
-    valid_line?(new_line, direction)
+    valid_line?(new_line)
   end
 end
 
 reduced_result = lines.map do |line|
   next 1 if line.length < 2
-
-  if line[1] > line[0]
-    valid_line?(line, :increasing) || valid_with_one_removal?(line, :increasing) ? 1 : 0
-  elsif line[1] < line[0]
-    valid_line?(line, :decreasing) || valid_with_one_removal?(line, :decreasing) ? 1 : 0
-  else
-    valid_with_one_removal?(line, :increasing) || valid_with_one_removal?(line, :decreasing) ? 1 : 0
-  end
+  valid_line?(line) || valid_with_one_removal?(line) ? 1 : 0
 end
 
 puts reduced_result.sum
